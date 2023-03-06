166d6a4febfd33880a30fe850e573d9f8c2e6ba3
Shoham <medinashoham@gmail.com>
2023-03-06 10:16:59 -0600

    clase student

--- src/TestStudent.java
+++ src/TestStudent.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class TestStudent {
+public class TestStudent {
+    public static void main(String[] args) {
+        System.out.println("Probando clase student");
+                Student Shoham25 = new Student (12345,"shoham","medina","ing.informatica",2);
+
+
+        Shoham25.mostrardatos();
+
+
+
+    }
 }
